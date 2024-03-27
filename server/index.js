d9983cfad8af5800c1f838c94a2cdb8f8530b175
yonava <yona@vossandreae.com>
2024-03-27 17:27:47 -0400

    modified some stuff on server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json('Hello World');
