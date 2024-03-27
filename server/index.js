9a3d2ea599ef9217a7b8775c70d4026003300a60
yonava <yona@vossandreae.com>
2024-03-27 17:46:38 -0400

    variable port

--- server/index.js
+++ server/index.js
@@ -13,6 +13,8 @@ app.get('/echo', (req, res) => {
   res.json(req.query);
 });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
   console.log('Server is running on port 3000');
 });
