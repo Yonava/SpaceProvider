caee20b989159d0778d8547f795c6417c1153af2
yonava <yona@vossandreae.com>
2024-03-27 19:43:32 -0400

    comments added

--- server/index.js
+++ server/index.js
@@ -1,20 +1,50 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mockData = require('./mock');
 
+// example of middleware you can use
+function myMiddleware(req, res, next) {
+  console.log('I am a middleware!');
+
+  // you can modify the request object here
+  req.query.example = 'hi from middleware!';
+
+  // this will be passed to the endpoint handler!
+
+  // call next to move on to the next middleware or endpoint handler if there are no more middleware functions
+  next();
+}
+
+// we tell our server to use the middleware every time a request comes in
+app.use(myMiddleware);
+
+// this lets other people access the uri of the server
+// and it stands for cross origin resource sharing (example of a middleware)
 app.use(cors());
+
+// this lets us parse the request body as json (another example of useful middleware!)
 app.use(express.json());
 
 app.get('/', (req, res) => {
+
+  // this will log out "hi from middleware!" in the servers console
+  console.log(req.example)
+
   res.json({
-    message: 'Welcome to the space provider API! 🚀. Try /echo!'
+    message: 'Welcome to the space provider API! 🚀. Try /room?id=LIB-350!'
   });
 });
 
-app.get('/echo', (req, res) => {
-  res.json(req.query);
+app.get('/room', (req, res) => {
+  // get the id from the query string
+  const { id } = req.query;
+  // if mockData[id] is null or undefined, return 'Room not found' otherwise return the room
+  const room = mockData[id] ?? 'Room not found';
+  res.json(room);
 });
 
+// ports are dynamically assigned in production, so we need to check if a port is provided by the environment
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
---
+++ server/mock.js
@@ -0,0 +1,20 @@
+
+const mock = {
+  'LIB-350': {
+    building: 'LIB',
+    roomCode: '350',
+    accessNotes: 'Please bring your own laptop',
+    images: 'something.png',
+    gps_coords: {
+      lat: 37.3352,
+      long: -121.8811
+    },
+    labels: ['WHITE_BOARD', 'QUIET_AREA'],
+    capacity: 50,
+    last_edited: new Date().toISOString(),
+  }
+}
+
+
+
+module.exports = mock
