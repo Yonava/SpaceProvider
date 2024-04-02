f37103ea051ffb498192de6d0999aa1df8a0c6c3
yonava <yona@vossandreae.com>
2024-04-02 16:01:28 -0400

    installed vuetify and setup admin rountes

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,11 +1,30 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
+// Vuetify
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    }
+  },
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(vuetify)
 
 app.mount('#app')
---
+++ server/api/admin.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+
+// admin endpoints
+
+module.exports = router;
