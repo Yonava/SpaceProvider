7f89578fda0af8d85da84221da8dee4dad038330
mrper <115744059+tomperature@users.noreply.github.com>
2024-04-18 14:37:21 -0400

    tomsImages.ts initial implementation

--- client/src/tomsImages.ts
+++ client/src/tomsImages.ts
@@ -1,57 +1,87 @@
+import imageCompression from "browser-image-compression";
+import heic2any from "heic2any";
 
 /**
- * @description Check if the file is an image format
+ * @description Check if the file is an accepted image format. 
  * @param file
- * @returns a boolean value that indicates if the file is an image format
+ * @returns a boolean value that indicates if the file is an accepted image format
  */
-const isImageFormat = (file: File): boolean => {
-  // TODO
+function isImageFormat(file: File): boolean {
+  const acceptedFormats = [
+    "image/jpeg", "image/png", "image/webp", "image/bmp", "image/heic", "image/heif"
+  ];
+  return acceptedFormats.some(f => f === file.type);
 }
 
 /**
- * @description Check if the image is a jpeg format
- * @param file
- * @returns a boolean value that indicates if the image is a jpeg format
+ * @description Converts inputted hei
+ * @param file 
+ * @returns 
  */
-const isJPEG = (file: File): boolean => {
-  // TODO
+async function convertHeicToJPEG(file: File): Promise<File> {
+  let convertedBlob = await heic2any({ 
+    blob: file, 
+    toType: "image/jpeg"
+  });
+  // if inputted heic file has multiple images, just take the first one
+  if (convertedBlob instanceof Array) { convertedBlob = convertedBlob[0]; }
+  return new File([convertedBlob], "", { type: "image/jpeg" });
 }
 
 /**
- * @description Convert the image to a jpeg format
+ * @description Check if the file is HEIC/HEIF format. 
  * @param file
+ * @returns a boolean value that indicates if the file is HEIC/HEIF format
+ */
+function isHEIC(file: File): boolean {
+  return file.type === "image/heic" || file.type === "image/heif";
+}
+
+/**
+ * @description Converts the image to a jpeg format, compressing it to be within a 
+ * certain file size. 
+ * @param file
+ * @param mbLimit
  * @returns a promise that resolves to a new File object with the jpeg format
  */
-
-const convertToJpeg = (file: File): Promise<File> => {
-  // TODO
+async function convertToJPEG(file: File, mbLimit: number): Promise<File> {
+  if (isHEIC(file)) {
+    file = await convertHeicToJPEG(file);
+  }
+  return await imageCompression(file, { 
+    maxSizeMB: mbLimit,
+    fileType: "image/jpeg"
+  });
 }
 
 /**
- * @description Resize the jpeg to a smaller size under a specified limit
- * @param file
- * @param limit
- * @returns a promise that resolves to a new File object with the resized jpeg
- */
-const resizeJPEG = (file: File, limit: number): Promise<File> => {
-  // TODO
-}
-
-/**
- * @description Encode the file to base64 format
+ * @description Encodes the file to base64 format.
  * @param file
  * @returns a promise that resolves to a string of the file encoded in base64
  */
-const encodeToBase64 = (file: File): Promise<string> => {
-  // TODO
+function encodeToBase64(file: File): Promise<string> {
+  const reader = new FileReader();
+  reader.readAsDataURL(file);
+  return new Promise<string>((resolve) => {
+    reader.onload = () => {
+      resolve(reader.result as string);
+    };
+  });
 }
 
 /**
- * @description takes a file and limit and converts said file to a jpeg format under the limit
+ * @description Takes a file and limit (in MB) and converts said file to a jpeg format under the 
+ * limit.
  * @param file
- * @param limit
+ * @param mbLimit
  * @returns a promise that resolves to a string of the file encoded in base64
  */
-const uploadImageFilePipeline = async (file: File, limit: number): Promise<string> => {
-  // TODO
+export async function uploadImageFilePipeline(file: File, mbLimit: number): Promise<string> {
+  try {  
+    if (!isImageFormat(file)) { throw new Error("File must be an image"); }
+    return await encodeToBase64(await convertToJPEG(file, mbLimit));
+  } catch(e) {
+    console.error("JPEG Conversion/compression failed:", e);
+    throw e;
+  }
 }
